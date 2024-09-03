b5946d34d7642f8f49e28bbac83f214e0e01ed99
diqia <diqiassiddiqi4@gmail.com>
2024-09-03 13:45:25 +0700

    Menambah fungsi input output

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+func inputOutput() { // menambah fungsi input output
+	fmt.Println("--------------")
+	var umur int
+	fmt.Print("Berapa umur anda? ")
+	fmt.Scan(&umur)
+	fmt.Print("Umur anda adalah ", umur)
+}
+
 func main() {
 	fmt.Print("Hello, World!\n")
 	fmt.Print("Nama saya Tafif\n")
 	fmt.Print("Saya Mahasiswa Informatika Angkatan 2021\n")
 	fmt.Print("Disini saya akan belajar mengenai Git dan Github\n")
+	inputOutput() // Memanggil fungsi input output
 }
