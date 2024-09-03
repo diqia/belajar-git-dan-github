4397caef8d60731ccb8a752820c5bd390c174ab8
diqia <diqiassiddiqi4@gmail.com>
2024-09-03 13:30:26 +0700

    First Github Project

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello, World!\n")
+	fmt.Print("Nama saya Tafif\n")
+	fmt.Print("Saya Mahasiswa Informatika Angkatan 2021\n")
+	fmt.Print("Disini saya akan belajar mengenai Git dan Github\n")
+}
